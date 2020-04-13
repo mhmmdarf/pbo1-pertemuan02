6bf0b094a41d5527172e082af049069c2f17a572
mhmmdarf <mhmmdarf4@gmail.com>
2020-04-13 21:06:42 +0800

    Memanggil method dari Class di package lain

---
+++ model/Dosen.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Dosen{
+ public static void tampilanInfo(){
+ System.out.println();
+ System.out.println("== Data Dosen ==");
+ System.out.println("NIDN : 1102931631");
+ System.out.println("Nama Dosen : Luhut Pandjaitan ");
+ System.out.println("================");
+ }
+}
